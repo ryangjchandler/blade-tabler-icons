a9d06b80c4eedf532588eef0841dd9fe909a419e
ryangjchandler <ryangjchandler@users.noreply.github.com>
2020-07-09 21:00:14 +0000

    Fix styling

--- src/BladeTablerIconsServiceProvider.php
+++ src/BladeTablerIconsServiceProvider.php
@@ -18,7 +18,7 @@ final class BladeTablerIconsServiceProvider extends ServiceProvider
             ]);
         });
     }
-        
+
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
