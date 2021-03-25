33886e988531d5ee684abfada727ecb24ecec0ed
ryangjchandler <ryangjchandler@users.noreply.github.com>
2021-03-25 13:23:39 +0000

    Fix styling

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -48,7 +48,7 @@ SVG;
     {
         $result = svg('tabler-accessible', ['style' => 'color: #555'])->toHtml();
 
-$expected = <<<SVG
+        $expected = <<<SVG
 <svg style="color: #555" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <circle cx="12" cy="12" r="9" />
   <path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1" />
