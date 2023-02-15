b891ebe715c108c89d262e1678831c685ce7e359
Ryan Chandler <ryangjchandler@gmail.com>
2023-02-15 14:12:08 +0000

    chore: update tests for new icons

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -18,7 +18,7 @@ class CompilesIconsTest extends TestCase
         // Note: the empty class here seems to be a Blade components bug.
         $expected = <<<SVG
 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-  <circle cx="12" cy="12" r="9" />
+  <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
   <path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1" />
   <circle cx="12" cy="7.5" r=".5" fill="currentColor" />
 </svg>
@@ -34,7 +34,7 @@ SVG;
 
         $expected = <<<SVG
         <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-          <circle cx="12" cy="12" r="9" />
+          <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
           <path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1" />
           <circle cx="12" cy="7.5" r=".5" fill="currentColor" />
         </svg>
@@ -50,7 +50,7 @@ SVG;
 
         $expected = <<<SVG
 <svg style="color: #555" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-  <circle cx="12" cy="12" r="9" />
+  <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
   <path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1" />
   <circle cx="12" cy="7.5" r=".5" fill="currentColor" />
 </svg>
