bd5fe4697d8ad7c1a816b313fd29be09678a04eb
Ngô Quốc Đạt <datlechin@gmail.com>
2024-02-17 22:08:15 +0700

    update

--- bin/auto-update.sh
+++ bin/auto-update.sh
@@ -2,23 +2,28 @@
 
 set -e
 
-latest_release_url=$(curl -s https://api.github.com/repos/tabler/tabler-icons/releases/latest | grep browser_download_url | cut -d '"' -f 4)
-repo_url="${latest_release_url}"
-extract_dir="resources/svg/"
-temp_zip="tabler-icons-master.zip"
+repo="tabler/tabler-icons"
+latest_release=$(curl -s https://api.github.com/repos/$repo/releases/latest)
+version=$(echo "$latest_release" | grep '"tag_name":' | cut -d'"' -f4 | sed 's/^v//')
+download_url=$(echo "$latest_release" | grep '"browser_download_url":' | cut -d'"' -f4)
+zip_file_name="tabler-icons-$version"
 
-echo "Downloading latest icons from $repo_url..."
-curl -Ls "$repo_url" -o "$temp_zip"
+echo "Downloading latest icons from https://github.com/$repo..."
+curl -OLs "$download_url"
 
 if [ $? -eq 0 ]; then
     echo "Downloaded successfully. Extracting..."
-    unzip -qqo "$temp_zip" -d "$extract_dir"
-    mv "$extract_dir/tabler-icons-master/icons/"* "$extract_dir"
-    rm -rf "$extract_dir/tabler-icons-master"
-    rm "$temp_zip"
+    unzip -qqo "$zip_file_name.zip" -d "$zip_file_name"
+
+    mkdir -p resources/svg
+    mv "$zip_file_name/svg"/* resources/svg
+
+    rm -rf "$zip_file_name"
+    rm "$zip_file_name.zip"
+
     echo "Cleaning up icon classes..."
 
-    for FILE in "$extract_dir"/*; do
+    for FILE in "resources/svg"/*.svg; do
         if [ -f "$FILE" ]; then
             tmp_file=$(mktemp) || exit 1
             sed -e 's/ class="[^\"]*"//g' -e '/<path stroke="none" d="M0 0h24v24H0z" fill="none"\/>/d' -e '/^$/d' "$FILE" >"$tmp_file" && mv "$tmp_file" "$FILE"
